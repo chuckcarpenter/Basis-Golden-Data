2c7c08752b841c2d0f02e69f1dd72fbfdcda5133
Chuck Carpenter <chuck.carpenter@me.com>
2014-03-10 19:21:29 -0400

    connected to get user info and authenticate

--- BGD.js
+++ BGD.js
@@ -1,35 +1,52 @@
-/*! BGD v0.0.0 - MIT license */
+var request = require('request');
+// var qs = require('querystring');
+// var jar = request.jar();
+// var r = request.post('http://service.com/upload');
+// var form = r.form();
 
-;(function (global) { function moduleDefinition(/*dependency*/) {
+// request.post('https://app.mybasis.com/login', {form: {username: 'juniormcbride@gmail.com', password: 'anywho21'}});
 
-// ---------------------------------------------------------------------------
 
-'use strict';
+// request('https://app.mybasis.com/login', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     //console.log(body) // Print the google web page.
+//     request.post('https://app.mybasis.com/login', {form: {username: 'juniormcbride@gmail.com', password: 'anywho21'}});
+//   }
+// })
 
-/**
- * @param {}
- * @return {}
- * @api public
- */
+// example URLs for API
+// for some reason, sleep must be submitted different than activities.
+//
+// https://app.mybasis.com/api/v2/users/me/days/2014-03-1/activities?type=sleep&expand=activities
+// https://app.mybasis.com/api/v1/chart/me?summary=true&interval=60&units=ms&start_date=2014-03-01&start_offset=-10800&end_offset=10800&heartrate=true&steps=true&calories=true&gsr=true&skin_temp=true&bodystates=true
+// https://app.mybasis.com/api/v2/users/me/days/2014-03-1/activities?type=run,walk,bike&expand=activities
 
-function BGD() {
-}
+request({
+    uri: 'https://app.mybasis.com/login',
+    method: 'POST',
+    form: {
+        username: 'juniormcbride@gmail.com',
+        password: 'anywho21'
+    },
+    //timeout: 10000,
+    followRedirect: true,
+    maxRedirects: 10,
+    jar: true
+}, function ( error, response, body ) {
+    // console.log(response.headers);
+    var access_token = response.headers['set-cookie'][0].match( /access_token=([0-9a-f]+)/ ),
+        // refresh_token = response.headers['set-cookie'][0].match( /refresh_token=([0-9a-f]+)/ ),
+        expires = response.headers['set-cookie'][0].match( /expires=([0-9a-f]+)/ );
 
-/**
- * Expose BGD
- */
-
-return BGD;
-
-// ---------------------------------------------------------------------------
-
-} if (typeof exports === 'object') {
-    // node export
-    module.exports = moduleDefinition(/*require('dependency')*/);
-} else if (typeof define === 'function' && define.amd) {
-    // amd anonymous module registration
-    define([/*'dependency'*/], moduleDefinition);
-} else {
-    // browser global
-    global.BGD = moduleDefinition(/*global.dependency*/);
-}}(this));
+    request.get({
+        url: 'https://app.mybasis.com/api/v1/user/me.json',
+        // oauth:access_token[1],
+        // headers: {
+        //     'X-Basis-Authorization': "OAuth:" + access_token[1]
+        // },
+        jar: access_token,
+        json: true
+    }, function (e, r, user) {
+        console.log( user );
+    });
+});
