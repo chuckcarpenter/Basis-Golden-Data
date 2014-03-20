64fa4e26ab684b73cf7c1001015589119a0b94e2
Chuck Carpenter <chuck.carpenter@me.com>
2014-03-19 20:41:48 -0400

    getting my psw out of the repo

--- BGD.js
+++ BGD.js
@@ -1,18 +1,8 @@
+'use strict';
+
 var request = require('request');
-// var qs = require('querystring');
-// var jar = request.jar();
-// var r = request.post('http://service.com/upload');
-// var form = r.form();
-
-// request.post('https://app.mybasis.com/login', {form: {username: 'juniormcbride@gmail.com', password: 'anywho21'}});
-
-
-// request('https://app.mybasis.com/login', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     //console.log(body) // Print the google web page.
-//     request.post('https://app.mybasis.com/login', {form: {username: 'juniormcbride@gmail.com', password: 'anywho21'}});
-//   }
-// })
+var fs = require('fs');
+var readline = require('readline');
 
 // example URLs for API
 // for some reason, sleep must be submitted different than activities.
@@ -21,18 +11,32 @@ var request = require('request');
 // https://app.mybasis.com/api/v1/chart/me?summary=true&interval=60&units=ms&start_date=2014-03-01&start_offset=-10800&end_offset=10800&heartrate=true&steps=true&calories=true&gsr=true&skin_temp=true&bodystates=true
 // https://app.mybasis.com/api/v2/users/me/days/2014-03-1/activities?type=run,walk,bike&expand=activities
 
-request({
-    uri: 'https://app.mybasis.com/login',
-    method: 'POST',
-    form: {
-        username: 'juniormcbride@gmail.com',
-        password: 'anywho21'
-    },
-    //timeout: 10000,
-    followRedirect: true,
-    maxRedirects: 10,
-    jar: true
-}, function ( error, response, body ) {
+var prompts = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var usr, psw;
+
+function askForUser() {
+    prompts.question('What is your Basis username? \n', function (answer) {
+        usr = answer;
+
+        askForPassword();
+    });
+}
+
+function askForPassword() {
+    prompts.question('What is your Basis password? \n', function (answer) {
+        psw = answer;
+
+        console.log(usr, psw);
+        requestData(usr, psw);
+        prompts.close();
+    });
+}
+
+function getToken( error, response, body ) {
     // console.log(response.headers);
     var access_token = response.headers['set-cookie'][0].match( /access_token=([0-9a-f]+)/ ),
         // refresh_token = response.headers['set-cookie'][0].match( /refresh_token=([0-9a-f]+)/ ),
@@ -47,6 +51,23 @@ request({
         jar: access_token,
         json: true
     }, function (e, r, user) {
-        console.log( user );
+        console.log(user);
     });
-});
+}
+
+function requestData(usr, psw) {
+    request({
+        uri: 'https://app.mybasis.com/login',
+        method: 'POST',
+        form: {
+            username: usr,
+            password: psw
+        },
+        //timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10,
+        jar: true
+    }, getToken);
+}
+
+askForUser();
